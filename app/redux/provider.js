16e3aa2f29c1e672705edabe90653d96cd160315
Zac Middleton <zachary.m_08@hotmail.com>
2023-06-29 19:04:15 -0700

    added persist gate

--- app/redux/provider.js
+++ app/redux/provider.js
@@ -1,8 +1,15 @@
 "use client";
 
-import { store } from './store/store';
-import { Provider } from 'react-redux';
+import { store, persistor } from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 export function ReduxProvider({ children }) {
-    return <Provider store={store} >{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
